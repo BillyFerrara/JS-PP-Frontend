e5973e10fdd9e30743307e4e16cdcd2aa2de13e9
Billy F <willfira717@gmail.com>
2020-12-21 15:25:13 -0500

    added review form / Event listener to index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 const productForm = document.getElementById('product-form')
+const productInput = document.getElementById('product-input')
+const productList = document.getElementById('product-list')
 
 productForm.addEventListener("submit", submitProduct);
 
-function submitProduct(){
+function submitProduct(e){
+    e.preventDefault()
     console.log("general kenobi")
+    console.log(productInput.value)
+
+    const li = document.createElement('li')
+    const p = document.createElement('p')
+    p.innerText = productInput.value
+    
+
+    const reviewForm = document.createElement('form')
+    reviewForm.innerHTML += `<input type="text" id="review-input"><input type="submit">`
+
+    reviewForm.addEventListener("submit", submitReview)
+
+    li.append(p, reviewForm)
+    productList.appendChild(li)
+
+    productForm.reset();
+    
+}
+
+function submitReview(e) {
+    e.preventDefault()
+    const reviewInput = e.target.children[0].value
+    console.log(reviewInput)
 }
